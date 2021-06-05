577b4c83fe3ba63c4144cbb79fc29757f555dc65
Maycon <maycontalmeida@gmail.com>
2021-06-05 17:43:35 -0300

    Projeto finalizado

---
+++ src/br/com/caelum/livraria/bean/LoginBean.java
@@ -0,0 +1,50 @@
+package br.com.caelum.livraria.bean;
+
+import javax.faces.application.FacesMessage;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
+import javax.faces.context.FacesContext;
+
+import br.com.caelum.livraria.dao.UsuarioDao;
+import br.com.caelum.livraria.modelo.Usuario;
+
+@ManagedBean
+@ViewScoped
+public class LoginBean {
+
+	private Usuario usuario = new Usuario();
+
+	public Usuario getUsuario() {
+		return usuario;
+	}
+
+	public void setUsuario(Usuario usuario) {
+		this.usuario = usuario;
+	}
+	
+	public String efetuarLogin() {
+		System.out.println("Fazendo login do usuário: " + this.usuario.getEmail());
+		
+		FacesContext context = FacesContext.getCurrentInstance();
+		boolean usuarioExiste = new UsuarioDao().existe(this.usuario);
+		
+		if (usuarioExiste) {
+			context.getExternalContext().getSessionMap().put("usuarioLogado", this.usuario);
+			return "livro?faces-redirect=true";
+		}
+		
+		context.getExternalContext().getFlash().setKeepMessages(true);
+		context.addMessage(null, new FacesMessage("O usuário não possui um cadastro em nosso sistema"));
+		
+		return "login?faces-redirect=true";
+		
+	}
+	
+	public String efetuarLogout() {
+		FacesContext context = FacesContext.getCurrentInstance();
+		context.getExternalContext().getSessionMap().remove("usuarioLogado");
+		
+		return "login?faces-redirect=true";
+	}
+
+}
---
+++ src/br/com/caelum/livraria/util/LogPhaseListener.java
@@ -0,0 +1,26 @@
+package br.com.caelum.livraria.util;
+
+import javax.faces.event.PhaseEvent;
+import javax.faces.event.PhaseId;
+import javax.faces.event.PhaseListener;
+
+public class LogPhaseListener implements PhaseListener {
+
+	private static final long serialVersionUID = 9039601385787424895L;
+
+	@Override
+	public void afterPhase(PhaseEvent event) {
+		
+	}
+
+	@Override
+	public void beforePhase(PhaseEvent event) {
+		
+	}
+
+	@Override
+	public PhaseId getPhaseId() {
+		return PhaseId.ANY_PHASE;
+	}
+
+}
