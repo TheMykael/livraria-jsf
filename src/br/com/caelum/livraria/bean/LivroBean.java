c9ebb147f1c25c1e3f2c63cd4c89ecfc74ba62a9
Maycon <maycontalmeida@gmail.com>
2021-06-07 22:40:39 -0300

    Adicionando Primefaces ao projeto

--- src/br/com/caelum/livraria/bean/LivroBean.java
+++ src/br/com/caelum/livraria/bean/LivroBean.java
@@ -1,114 +1,18 @@
 package br.com.caelum.livraria.bean;
 
-import java.io.Serializable;
-import java.util.List;
-
-import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
-import javax.faces.bean.ViewScoped;
-import javax.faces.component.UIComponent;
-import javax.faces.context.FacesContext;
-import javax.faces.validator.ValidatorException;
-
-import br.com.caelum.livraria.dao.DAO;
-import br.com.caelum.livraria.modelo.Autor;
-import br.com.caelum.livraria.modelo.Livro;
 
 @ManagedBean
-@ViewScoped
-public class LivroBean implements Serializable {
-
-	private static final long serialVersionUID = -936001793792060104L;
+public class LivroBean {
 	
 	private Livro livro = new Livro();
-	private Integer autorId;
-	private Integer livroId;
-	
-	public Integer getAutorId() {
-		return autorId;
-	}
-	
-	public void setAutorId(Integer autorId) {
-		this.autorId = autorId;
-	}
 
 	public Livro getLivro() {
 		return livro;
 	}
-	
-	public List<Livro> getLivros() {
-		return new DAO<Livro>(Livro.class).listaTodos();
-	}
-	
-	public List<Autor> getAutores() {
-		return new DAO<Autor>(Autor.class).listaTodos();
-	}
-	
-	public Integer getLivroId() {
-		return livroId;
-	}
-
-	public void setLivroId(Integer livroId) {
-		this.livroId = livroId;
-	}
-	
-	public void carregarLivroPeloId() {
-		this.livro = new DAO<Livro>(Livro.class).buscaPorId(livroId);
-	}
 
 	public void gravar() {
-		System.out.println("Gravando livro " + this.livro.getTitulo());
-
-		if (livro.getAutores().isEmpty()) {
-			FacesContext.getCurrentInstance()
-			.addMessage("autor", new FacesMessage("O livro deve ter pelo menos um autor"));
-			return;
-		}
-		
-		if (this.livro.getId() == null) {
-			new DAO<Livro>(Livro.class).adiciona(this.livro);
-		} else {
-			new DAO<Livro>(Livro.class).atualiza(this.livro);
-		}
-
-		this.livro = new Livro();
-	}
-	
-	public void remover(Livro livro) {
-		System.out.println("Removendo livro");
-		new DAO<Livro>(Livro.class).remove(livro);
-	}
-	
-	public void removerAutor(Autor autor) {
-		System.out.println("Removendo autor");
-		this.livro.removeAutor(autor);
-	}
-	
-	public void carregar(Livro livro) {
-		System.out.println("Carregando livro");
-		this.livro = livro;
-	}
-	
-	public void gravarAutor() {
-		Autor autor = new DAO<Autor>(Autor.class).buscaPorId(this.autorId);
-		this.livro.adicionaAutor(autor);
-		System.out.println("Livro escrito por: " + autor.getNome());
-	}
-	
-	public List<Autor> getAutoresDoLivro() {
-		return this.livro.getAutores();
-	}
-	
-	public String formAutor() {
-		System.out.println("Chamando o formul�rio do autor");
-		return "autor?faces-redirect=true";
-	}
-	
-	public void comecaComDigitoUm(FacesContext fc, UIComponent component, Object value) throws ValidatorException {
-		String valor = value.toString();
-		if (!valor.startsWith("1")) {
-			throw new ValidatorException(new FacesMessage("O ISBN deve come�ar com 1"));
-		}
+		System.out.println("Gravando livro: " + this.livro.getTitulo());
 	}
 
 }
